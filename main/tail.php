627cc91cf110e733b9cf0de1a6e253be85c71166
KH <wedn@wedn.io>
2024-02-05 08:44:57 +0900

    tail update

--- main/tail.php
+++ main/tail.php
@@ -1,8 +1,8 @@
 	<? if($_DEV){ ?>
 		<div class="dev-info">
 			<?=DEV_PATH($INCLUDE_CONTENTS)?>
-			<?=DEV_PARAMETER($_POST, $_GET)?>
 			<?=DEV_INFO()?>
+			<?=DEV_PARAMETER($_POST, $_GET)?>
 		</div>
 	<? } ?>
 </body>
