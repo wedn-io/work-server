105e3eadb703a2c0f7bc9dd303b137d02e2737f2
KH <wedn@wedn.io>
2024-02-05 17:56:06 +0900

    working...

--- common.php
+++ common.php
@@ -66,7 +66,7 @@
 
 	function DEV_SESSION(){
 		$RESPONSE_SESSION = "[SESSION]";
-		$RESPONSE_SESSION = "<br>";
+		$RESPONSE_SESSION .= "<br>";
 		foreach($_SESSION as $k => $v){
 			$RESPONSE_SESSION .= "[". $k . " : " . $v . "]<br>";
 		}
