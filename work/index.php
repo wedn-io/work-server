d116d0f55d9a58ba445376b367c8ba36388baae6
KH <wedn@wedn.io>
2024-02-05 14:28:27 +0900

    working...

--- work/index.php
+++ work/index.php
@@ -7,12 +7,12 @@
 
 <h2>TODOLIST</h2>
 <textarea name="todo-contents"></textarea>
-<button type="button" onChange="todolist_registration()">Add</button>
+<button type="button" onClick="todolist_registration()">Add</button>
 
 <script>
 	function todolist_registration(){
 		alert("@@");
-		let todo_contents = document.getElementsByName("todo-contents");
+		let todo_contents = document.getElementsByName("todo-contents")[0].value;
 		console.log(todo_contents);
 	}
 </script>
